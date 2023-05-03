f5c93e5c412e6a8e6f82bdd321c19d337622daf1
Alex G <63746732+itsalexg87@users.noreply.github.com>
2023-05-03 12:09:30 +0200

    another fix server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import dalleRoutes from "./routes/dalleRoutes.js";
 dotenv.config();
 
 const app = express();
+app.use(express.static("public"));
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1/posts", postRoutes);
@@ -30,3 +31,4 @@ const startServer = async () => {
 };
 
 startServer();
+module.exports = app;
